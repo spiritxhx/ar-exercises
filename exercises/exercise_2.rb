51e2b62e92404c87e2bbd1d513dc8d73ef83f087
spiritxhx <spiritxhx@gmail.com>
2019-08-19 16:57:06 -0400

    complete the update and delete function

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,8 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+
+@store1 = Stores.find_by(id: 1)
+@store2 = Stores.find_by(id: 2)
+@store1.name = "Montreal"
+@store1.save
--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -6,3 +6,6 @@ puts "Exercise 3"
 puts "----------"
 
 # Your code goes here ...
+
+store3 = Stores.find_by(id: 3)
+store3.destroy
