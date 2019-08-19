43fbb630ed153b1b329532528e92a85e3d9e8059
spiritxhx <spiritxhx@gmail.com>
2019-08-19 16:45:00 -0400

    complete the 1st exercise

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,11 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+class Stores < ActiveRecord::Base
+end
+
+stores = Stores.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
+stores = Stores.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
+stores = Stores.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
+
+puts Stores.count
